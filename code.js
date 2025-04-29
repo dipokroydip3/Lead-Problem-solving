ddc9d53443397abc32c04aa64ce04ce1237033c0
dipokroydip3 <dipokroydip3@gmail.com>
2025-04-29 11:41:47 +0600

    first commit

---
+++ code.js
@@ -0,0 +1,53 @@
+// const newFunction = (others) => {
+//     let justNum = 10;
+//     let secondNumber = 20;
+//     const totalCount = justNum + secondNumber;
+//     console.log(others - totalCount);
+    
+// }
+
+// newFunction(50)
+
+
+// function groupAnagrams(words) {
+//     const map = new Map();
+  
+//     for (let word of words) {
+//       // Sort letters to form a key
+//       const sorted = word.split('').sort().join('');
+      
+//       if (!map.has(sorted)) {
+//         map.set(sorted, []);
+//       }
+//       map.get(sorted).push(word);
+//     }
+  
+//     return Array.from(map.values());
+//   }
+  
+//   // Example usage:
+//   const input = ["eat", "tea", "tan", "ate", "nat", "bat"];
+//   console.log(groupAnagrams(input));
+  
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const secondFruits = fruits.length;
+// console.log(secondFruits);
+
+// function myFunction(p1, p2) {
+//     const multiple =  p1 * p2;
+    
+//     console.log(multiple);
+    
+//   }
+  
+
+//   myFunction(10,20);
+
+// let maxNumber = [ 20, 40, 60, 100 ];
+// let finalMaxNumer = Math.max(...maxNumber);
+// console.log(finalMaxNumer);
+
+
+
