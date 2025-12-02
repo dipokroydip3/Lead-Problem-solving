c944a16ee1c0c8ddfefb6ab3ec4863d598afb844
Dip <dipokroydip3@gmail.com>
2025-12-02 23:26:22 +0600

    solve one more problem

---
+++ problem6.js
@@ -0,0 +1,31 @@
+// Problem: Find the Longest Word in a Sentence
+
+// Given a sentence (string), return the longest word.
+// If two words have the same length, return the first one.
+
+// Example
+
+// Input:
+// "I am learning JavaScript programming"
+
+// Output:
+// "JavaScript"
+// (because it has 10 letters)
+
+
+
+
+function longestWord(sentence) {
+    const words = sentence.split(" ");  // split by space
+    let longest = "";
+
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestWord("I am learning JavaScript programming"));
