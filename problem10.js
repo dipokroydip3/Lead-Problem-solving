f3683a75cfcc009a9a36aa58e780370c9a58d0b3
Dip <dipokroydip3@gmail.com>
2025-12-08 21:11:26 +0600

    solve a simple problem

---
+++ problem10.js
@@ -0,0 +1,13 @@
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiouAEIOU";
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("hello world")); // Output: 3
