30b2aa7039d254c07b00f58e37cb49a12b6836df
Dip <dipokroydip3@gmail.com>
2025-12-11 17:35:25 +0600

    solve the problem another away

--- problem11.js
+++ problem11.js
@@ -1,13 +1,26 @@
-function filterOddGreaterThanTen(arr) {
-  let result = [];
+// function filterOddGreaterThanTen(arr) {
+//   let result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 10 && arr[i] % 2 === 1) {
-      result.push(arr[i]);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 10 && arr[i] % 2 === 1) {
+//       result.push(arr[i]);
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(filterOddGreaterThanTen([5, 12, 17, 9, 21, 30, 41]));
+
+function filterEvenGreterThantTen (arr2) {
+  let result2 = [];
+
+  for (let i = 0; i < arr2.length; i++) {
+    if(arr2[i] > 10 && arr2[i] % 2 == 0) {
+      result2.push(arr2[i]);
     }
   }
-
-  return result;
+  return result2;
 }
 
-console.log(filterOddGreaterThanTen([5, 12, 17, 9, 21, 30, 41]));
+console.log(filterEvenGreterThantTen([5, 12, 15, 20, 45, 21]));
