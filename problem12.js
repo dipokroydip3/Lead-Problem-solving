8ad08b6bce805ef0dbf5ece921e360e00ce3ed32
Dip <dipokroydip3@gmail.com>
2025-12-12 20:50:31 +0600

    solve the problem another away

--- problem12.js
+++ problem12.js
@@ -1,24 +1,43 @@
-function dayName(number) {
-    switch (number) {
-        case 1:
-            return "Sunday";
-        case 2:
-            return "Monday";
-        case 3:
-            return "Tuesday";
-        case 4:
-            return "Wednesday";
-        case 5:
-            return "Thursday";
-        case 6:
-            return "Friday";
-        case 7:
-            return "Saturday";
+// function dayName(number) {
+//     switch (number) {
+//         case 1:
+//             return "Sunday";
+//         case 2:
+//             return "Monday";
+//         case 3:
+//             return "Tuesday";
+//         case 4:
+//             return "Wednesday";
+//         case 5:
+//             return "Thursday";
+//         case 6:
+//             return "Friday";
+//         case 7:
+//             return "Saturday";
+//         default:
+//             return "Invalid day";
+//     }
+// }
+
+// // Example:
+// console.log(dayName(3)); 
+// console.log(dayName(9)); 
+
+
+
+function checkVowel(letter) {
+    switch (letter.toLowerCase()) {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            return "Vowel";
         default:
-            return "Invalid day";
+            return "Consonant";
     }
 }
 
-// Example:
-console.log(dayName(3)); 
-console.log(dayName(9)); 
+console.log(checkVowel("a"));
+console.log(checkVowel("b")); 
+
