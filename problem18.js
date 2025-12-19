2e3815ca8bc99e4c64eb39cf86262fe5b589b737
Dip <dipokroydip3@gmail.com>
2025-12-20 00:41:05 +0600

    solve a simple problem using loop for find the even and odd number

--- problem18.js
+++ problem18.js
@@ -1,9 +1,31 @@
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-let sum = 0;
+// let sum = 0;
+
+// for(let i = 0; i < arr.length; i++){
+//      sum += arr[i];
+// }
+
+// console.log(sum);
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let even = [];
+let odd = [];
 
 for(let i = 0; i < arr.length; i++){
-     sum += arr[i];
+     if(arr[i] % 2 === 0){
+          even.push(arr[i]);
+     }
+     else{
+          odd.push(arr[i]);
+     }
 }
+console.log(even);
+console.log(odd);
 
-console.log(sum);
+
+
+
+
