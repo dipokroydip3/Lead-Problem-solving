a4a907eef47d948a893d26cdee93be119f0b5c2f
Dip <dipokroydip3@gmail.com>
2025-12-20 00:52:02 +0600

    find the even and odd sum

--- problem18.js
+++ problem18.js
@@ -11,15 +11,16 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let even = [];
-let odd = [];
+let even = 0;
+let odd = 0;
 
 for(let i = 0; i < arr.length; i++){
      if(arr[i] % 2 === 0){
-          even.push(arr[i]);
+          
+          even =+ arr[i];
      }
      else{
-          odd.push(arr[i]);
+          odd =+ arr[i];
      }
 }
 console.log(even);
