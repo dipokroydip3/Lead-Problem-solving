ac1e0bdea295604b06c52f9180cfc112f4624918
Dip <dipokroydip3@gmail.com>
2025-12-20 23:37:49 +0600

    find the even and odd number using filter and reduce

--- problem18.js
+++ problem18.js
@@ -9,22 +9,37 @@
 // console.log(sum);
 
 
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let even = 0;
+// let odd = 0;
+
+// for(let i = 0; i < arr.length; i++){
+//      if(arr[i] % 2 === 0){
+          
+//           even =+ arr[i];
+//      }
+//      else{
+//           odd =+ arr[i];
+//      }
+// }
+// console.log(even);
+// console.log(odd);
+
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let even = 0;
-let odd = 0;
+let evenSum = arr
+  .filter(num => num % 2 === 0)
+  .reduce((sum, num) => sum + num, 0);
 
-for(let i = 0; i < arr.length; i++){
-     if(arr[i] % 2 === 0){
-          
-          even =+ arr[i];
-     }
-     else{
-          odd =+ arr[i];
-     }
-}
-console.log(even);
-console.log(odd);
+let oddSum = arr
+  .filter(num => num % 2 !== 0)
+  .reduce((sum, num) => sum + num, 0);
+
+console.log("Even Sum:", evenSum);
+console.log("Odd Sum:", oddSum);
+
 
 
 
