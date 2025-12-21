0793deee3912615642a5943de9c7db18f35b0b52
Dip <dipokroydip3@gmail.com>
2025-12-21 23:43:59 +0600

    solve a new problem using for in loop

--- problem18.js
+++ problem18.js
@@ -27,18 +27,46 @@
 // console.log(odd);
 
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let evenSum = arr
-  .filter(num => num % 2 === 0)
-  .reduce((sum, num) => sum + num, 0);
+// let evenSum = arr
+//   .filter(num => num % 2 === 0)
+//   .reduce((sum, num) => sum + num, 0);
 
-let oddSum = arr
-  .filter(num => num % 2 !== 0)
-  .reduce((sum, num) => sum + num, 0);
+// let oddSum = arr
+//   .filter(num => num % 2 !== 0)
+//   .reduce((sum, num) => sum + num, 0);
 
-console.log("Even Sum:", evenSum);
-console.log("Odd Sum:", oddSum);
+// console.log("Even Sum:", evenSum);
+// console.log("Odd Sum:", oddSum);
+
+
+
+
+
+function findDuplicates(arr) {
+    let freq = {};
+    let duplicates = {};
+
+    // frequency count
+    for (let index in arr) {
+        let value = arr[index];
+        freq[value] = (freq[value] || 0) + 1;
+    }
+
+    // filter duplicates
+    for (let key in freq) {
+        if (freq[key] > 1) {
+            duplicates[key] = freq[key];
+        }
+    }
+
+    return duplicates;
+}
+
+// Test
+console.log(findDuplicates([2, 3, 4, 2, 5, 3, 6, 3]));
+
 
 
 
