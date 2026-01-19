67806aa2efd0a6519b06e2d1d842316dfadf7b5b
Dip <dipokroydip3@gmail.com>
2026-01-19 22:56:31 +0600

    solve a new problem with step 1

--- problem20.js
+++ problem20.js
@@ -1,8 +1,28 @@
-function strongLogicSum(arr) {
-  let unique = [];
-  let sum = 0;
+// function strongLogicSum(arr) {
+//   let unique = [];
+//   let sum = 0;
 
-  // Step 1: unique positive numbers
+//   // Step 1: unique positive numbers
+//   for (let i = 0; i < arr.length; i++) {
+//     let num = arr[i];
+
+//     if (num > 0 && unique.indexOf(num) === -1) {
+//       unique.push(num);
+//     }
+//   }
+//    return sum;
+// }
+
+// // Example
+// console.log(strongLogicSum([12, 21, 30, 12, -3, 10, 1])); // 41
+
+
+
+function findSmallestEvenDigitSum(arr) {
+  let unique = [];
+  let smallest = null;
+
+  // Step 1: collect unique positive numbers
   for (let i = 0; i < arr.length; i++) {
     let num = arr[i];
 
@@ -10,8 +30,13 @@ function strongLogicSum(arr) {
       unique.push(num);
     }
   }
-   return sum;
+
+  // Step 2: check digit sum even
+  
+
+  return smallest === null ? "Not Found" : smallest;
 }
 
 // Example
-console.log(strongLogicSum([12, 21, 30, 12, -3, 10, 1])); // 41
+let numbers = [12, -4, 22, 15, 12, 8, 33, 44];
+console.log(findSmallestEvenDigitSum(numbers)); // 22
