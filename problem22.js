bddb88cbc0db8fe8082f182b67c02b0cc71de557
Dip <dipokroydip3@gmail.com>
2026-02-27 23:25:13 +0600

    try to solve the problem

--- problem22.js
+++ problem22.js
@@ -1,18 +1,38 @@
-const events = [9, 8, 7, 2, 12, 23, 4];
+// const events = [9, 8, 7, 2, 12, 23, 4];
 
-const findEven = [];
+// const findEven = [];
 
-for(let i = 0; i < events.length; i++) {
-     if(events[i] % 2 === 0)
-          findEven.push(events[i]);
-}
+// for(let i = 0; i < events.length; i++) {
+//      if(events[i] % 2 === 0)
+//           findEven.push(events[i]);
+// }
 
-let sum = 0;
+// let sum = 0;
 
-for(let i = 0; i < findEven.length; i++){
-     sum += findEven[i];
-}
+// for(let i = 0; i < findEven.length; i++){
+//      sum += findEven[i];
+// }
 
-console.log(findEven);
-console.log("Total even sum =", sum);
+// console.log(findEven);
+// console.log("Total even sum =", sum);
+
+
+const students = [
+  { name: "Rahim", marks: 80 },
+  { name: "Karim", marks: 35 },
+  { name: "Salam", marks: 50 },
+  { name: "Jamal", marks: 20 }
+];
+
+// 1. Passed Students
+const passedStudents = students.filter(student => student.marks > 40);
+
+// 2. Add Status
+const updatedStudents = students.map(student => ({
+  ...student,
+  status: student.marks > 40 ? "Passed" : "Failed"
+}));
+
+// 3. Average Marks
+
 
