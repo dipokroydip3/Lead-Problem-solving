48c2ee71652239d7dfc23a70338a83e545692c18
Dip <dipokroydip3@gmail.com>
2026-02-28 13:33:12 +0600

    Two Sum problem solved with o(n) time complexity

--- problem22.js
+++ problem22.js
@@ -17,27 +17,49 @@
 // console.log("Total even sum =", sum);
 
 
-const students = [
-  { name: "Rahim", marks: 80 },
-  { name: "Karim", marks: 35 },
-  { name: "Salam", marks: 50 },
-  { name: "Jamal", marks: 20 }
-];
+// const students = [
+//   { name: "Rahim", marks: 80 },
+//   { name: "Karim", marks: 35 },
+//   { name: "Salam", marks: 50 },
+//   { name: "Jamal", marks: 20 }
+// ];
 
-// 1. Passed Students
-const passedStudents = students.filter(student => student.marks > 40);
+// // 1. Passed Students
+// const passedStudents = students.filter(student => student.marks > 40);
 
-// 2. Add Status
-const updatedStudents = students.map(student => ({
-  ...student,
-  status: student.marks > 40 ? "Passed" : "Failed"
-}));
+// // 2. Add Status
+// const updatedStudents = students.map(student => ({
+//   ...student,
+//   status: student.marks > 40 ? "Passed" : "Failed"
+// }));
 
-// 3. Average Marks
-const totalMarks = students.reduce((sum, student) => sum + student.marks, 0);
-const average = totalMarks / students.length;
+// // 3. Average Marks
+// const totalMarks = students.reduce((sum, student) => sum + student.marks, 0);
+// const average = totalMarks / students.length;
 
-console.log(passedStudents);
-console.log(updatedStudents);
-console.log("Average:", average);
+// console.log(passedStudents);
+// console.log(updatedStudents);
+// console.log("Average:", average);
+
+
+var twoSum = function(nums, target) {
+    const map = {};
+
+    for(let i = 0; i < nums.length; i++){
+        const complement = target - nums[i];
+
+        if(map[complement] !== undefined){
+            return [map[complement], i];
+        }
+
+        map[nums[i]] = i;
+    }
+};
+
+const nums = [2,7,11,15];
+const target = 9;
+
+const result = twoSum(nums, target);
+console.log(result);
+
 
