55b2262d0ff243a65bb778ea34b97fee6fe09d2b
Dip <dipokroydip3@gmail.com>
2026-03-04 16:27:38 +0600

    try to solve triple sum problem

--- problem22.js
+++ problem22.js
@@ -41,6 +41,7 @@
 // console.log(updatedStudents);
 // console.log("Average:", average);
 
+// -----------------------------------------------------------------------
 
 // var twoSum = function(nums, target) {
 //     const map = {};
@@ -62,6 +63,7 @@
 // const result = twoSum(nums, target);
 // console.log(result);
 
+// ------------------------------------------------------------------
 
 // var longestCommonPrefix = function(strs) {
 //     if(strs.length === 0) return "";
@@ -82,25 +84,67 @@
 // const needOut = ["fl"];
 // console.log(strs.length);
 
+// --------------------------------------------------------------
 
-var longestCommonPrefix = function(strs) {
-    if(strs.length === 0) return "";
+// var longestCommonPrefix = function(strs) {
+//     if(strs.length === 0) return "";
 
-    let prefix = strs[0];
+//     let prefix = strs[0];
 
-    for(let i = 1; i < strs.length; i++) {
-        while(strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-            if(prefix === "") return "";
+//     for(let i = 1; i < strs.length; i++) {
+//         while(strs[i].indexOf(prefix) !== 0) {
+//             prefix = prefix.substring(0, prefix.length - 1);
+//             if(prefix === "") return "";
+//         }
+//     }
+//     return prefix;
+
+// }
+
+// const strs = ["flower","flow","flight"];
+// const result = longestCommonPrefix(strs)
+// console.log(result);
+
+
+// ----------------------------------------------------------
+
+
+var threeSum = function(nums) {
+    const result = [];
+    
+    // Step 1: Sort
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        
+        // Step 2: Skip duplicate fixed element
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                
+                // Skip duplicate left
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                
+                // Skip duplicate right
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                
+                left++;
+                right--;
+                
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
         }
     }
-    return prefix;
-
-}
-
-const strs = ["flower","flow","flight"];
-const result = longestCommonPrefix(strs)
-console.log(result);
-
-
-
+    
+    return result;
+};
